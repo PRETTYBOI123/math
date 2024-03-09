335023c6073e0b3876e62bedfaa3dd13c937f42f
PRETTYBOI123 <mclovern25@gmail.com>
2024-03-09 15:58:20 +0800

    Addition

---
+++ Index.php
@@ -0,0 +1,15 @@
+<html>
+
+      Prince Palad
+    <p> ADET BSIT - IT </p>
+    <form action="welcome_get.php" method="GET">
+input1: <input type="text" name="input1"><br>
+input2: <input type="text" name="input2"><br>
+<input type="submit">
+</form>
+    
+    <body>
+     
+    
+</body>
+</html>
---
+++ math.php
@@ -0,0 +1,8 @@
+<?php
+$input1 = $_POST["input1"];
+$input2 = $_POST["input2"];
+
+$output =0;
+$output = $input1 + $input2;
+echo "The Sum is:". $output;
+?>
